809285695012d1b5ba041d756acec042e30a63d1
ismetkync <ismetkync737@gmail.com>
2023-04-03 19:46:37 +0300

    db config

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const path = require("path")
 
 const errorHandler = require("./middlewares/errorMiddleware")
 
@@ -10,9 +11,18 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to the Survey App API" })
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  })
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to the Survey App API" })
+  })
+
+}
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"))
